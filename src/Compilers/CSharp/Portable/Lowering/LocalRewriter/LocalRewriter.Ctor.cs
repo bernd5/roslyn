ba82e13b59ec41784aa0bc9de2f654ee78db579e
bernd <baumannsbernd@gmail.com>
2023-12-08 23:59:01 +0100

    add LocalRewriter public ctor

---
+++ src/Compilers/CSharp/Portable/Lowering/LocalRewriter/LocalRewriter.Ctor.cs
@@ -0,0 +1,47 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Microsoft.CodeAnalysis.CodeGen;
+using Microsoft.CodeAnalysis.CSharp.Emit;
+using Microsoft.CodeAnalysis.CSharp.Symbols;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CodeAnalysis.PooledObjects;
+using Microsoft.CodeAnalysis.RuntimeMembers;
+using Roslyn.Utilities;
+
+namespace Microsoft.CodeAnalysis.CSharp
+{
+    partial class LocalRewriter
+    {
+        public LocalRewriter(
+            Compilation compilation,
+            MethodSymbol containingMethod,
+            int containingMethodOrdinal,
+            BoundStatement rootStatement,
+            NamedTypeSymbol? containingType,
+            SyntheticBoundNodeFactory factory,
+            SynthesizedSubmissionFields previousSubmissionFields,
+            bool allowOmissionOfConditionalCalls,
+            BindingDiagnosticBag diagnostics
+            ) : this((CSharpCompilation)compilation,
+             containingMethod,
+             containingMethodOrdinal,
+             rootStatement,
+             containingType,
+             factory,
+             previousSubmissionFields,
+             allowOmissionOfConditionalCalls,
+             diagnostics)
+        {
+            //
+        }
+    }
+}
